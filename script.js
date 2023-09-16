082eeaa045903584328e2f692c32bb3f646f4fe3
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-16 08:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let box = document.getElementById('browser-info');
+let Agent = navigator.userAgent;
+let Name = navigator.appName;
+let Version = navigator.appVersion;
+box.innerHTML = `You are using ${Name} version ${Version}`;
